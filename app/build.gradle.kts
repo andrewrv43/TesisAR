a3b86437b2b6648300e90aa708e6f070faffdb9b
ronny <rroserof@est.ups.edu.ec>
2024-09-08 03:18:46 -0500

    Test Google Maps v0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,9 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps)
+    implementation(libs.androidx.fragment)
+    implementation("com.google.android.gms:play-services-maps:17.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
-
 }
--- app/src/main/java/ups/tesis/detectoraltavelocidad/MapsActivity.kt
+++ app/src/main/java/ups/tesis/detectoraltavelocidad/MapsActivity.kt
@@ -5,28 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
 class MapsActivity : AppCompatActivity() ,OnMapReadyCallback{
-    lateinit var  maps: GoogleMap
+    private lateinit var maps: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_maps)
+        createFragment()
+        /*
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val mapfragment:SupportMapFragment=supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
-        mapfragment.getMapAsync(this)
+        */
     }
 
-    override fun onMapReady(p0: GoogleMap) {
-        maps = p0
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment? = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        maps = googleMap
         // Añadir un marcador en una ubicación específica
 //        val sydney = LatLng(-34.0, 151.0)
 //        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))

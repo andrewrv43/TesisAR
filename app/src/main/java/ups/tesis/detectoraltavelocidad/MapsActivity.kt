4b3d81a724fde54e25e57bf7bdc08a4f2c602a09
ronny <rroserof@est.ups.edu.ec>
2024-09-08 19:45:49 -0500

    Localizacion en timepo real v0.12

--- app/src/main/java/ups/tesis/detectoraltavelocidad/MapsActivity.kt
+++ app/src/main/java/ups/tesis/detectoraltavelocidad/MapsActivity.kt
@@ -1,23 +1,37 @@
 package ups.tesis.detectoraltavelocidad
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MapsActivity : AppCompatActivity() ,OnMapReadyCallback{
-    private lateinit var maps: GoogleMap
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMapClickListener {
+    private lateinit var map: GoogleMap
+    companion object { const val REQUEST_CODE_LOCATION = 0 }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.setOnMyLocationClickListener(this)
+        map.setOnMapClickListener(this)
+        enableLocation()
+        // Añadir un marcador en una ubicación específica
+        // val sydney = LatLng(-34.0, 151.0)
+        // map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        // map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        createFragment()
+        createMapFragment()
         /*
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -28,16 +42,67 @@ class MapsActivity : AppCompatActivity() ,OnMapReadyCallback{
         */
     }
 
-    private fun createFragment() {
-        val mapFragment: SupportMapFragment? = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment?.getMapAsync(this)
+    /**
+     * Crear fragmento del mapa
+     */
+    private fun createMapFragment() {
+        val mapFragment: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        maps = googleMap
-        // Añadir un marcador en una ubicación específica
-//        val sydney = LatLng(-34.0, 151.0)
-//        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    /**
+     *  Verifica si existen permisos de localizacion aceptados
+     */
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    /**
+     *  Activar los permisos de Localizacion
+     */
+    private fun enableLocation() {
+        if(!::map.isInitialized) return
+        if(isLocationPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    /**
+     *  Pide los permisos de Localizacion al usuario
+     */
+    private fun requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "Acepta los permisos en ajustes", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                map.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(this, "Para aceptar los permisos debes hacerlo desde ajustes", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Localizacion: ${p0.latitude} , ${p0.longitude}", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onMapClick(p0: LatLng) {
+        Toast.makeText(this, "Localizacion de mapa: ${p0.latitude} , ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
 }

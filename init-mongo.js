42c02f59a7696103e8cf0804a0e90e148fade999
Andrew <paul.rueda@hotmail.com>
2024-10-24 23:35:34 -0500

    test help

--- init-mongo.js
+++ init-mongo.js
@@ -5,6 +5,11 @@ db.createUser({
     roles: [ { role: "root", db: "admin" } ]
 });
 db = db.getSiblingDB('sp_base');
+db.createUser({
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    roles: [ { role: "root", db: "admin" } ]
+});
 db.createCollection('sp_users');
 db.createCollection('sp_record');
 db.createCollection('sp_backup');

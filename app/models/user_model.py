4e7ff9f2d745df76d4a2415181bd69b3111c3079
Andrew <paul.rueda@hotmail.com>
2024-11-14 01:07:35 -0500

    fix direccion

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -91,7 +91,7 @@ class SpeedRecord:
     
     @staticmethod
     def get_records_by_user(userid: str):
-        records = db['sp_record'].find({'userid': userid})
+        records = db['sp_record'].find({'userid': userid}).sort('_id',-1)
         return [
             {
                 'id': str(record['_id']),

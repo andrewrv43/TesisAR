4b2a8e0221e69ab5872bf41fdd294a83392fdfd4
ronny <rroserof@est.ups.edu.ec>
2024-11-14 20:50:37 -0500

    v0.25.1 Refactorizacion InfoActivity

---
+++ app/src/main/java/ups/tesis/detectoraltavelocidad/conexionec2/models/getPreferences.kt
@@ -0,0 +1,8 @@
+package ups.tesis.detectoraltavelocidad.conexionec2.models
+
+data class MapConfig(
+    val roadSelection: Int,
+    val landmarkSelection: Int,
+    val labelSelection: Int,
+    val darkSwitch: Boolean
+)

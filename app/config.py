b16a41bb87d71613f9123142d359dec024bedd3b
Andrew <paul.rueda@hotmail.com>
2024-12-13 22:50:19 -0500

    testURI

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config:
     MONGO_URI = os.getenv('MONGO_URI')
     SECRET_KEY = os.getenv('pwdLocal')
     email=EmailSender(os.getenv('pwdmail'))
+    print(MONGO_URI)
     @staticmethod
     def init_app(app):
         Config.email.sendEmail("Service up","Se levantó el servicio con Exito")
